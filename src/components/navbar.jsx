30f6ff774aab064f576533ca1fe1a961165c3155
Mohammed Kamaldeen <mohammedkamaldeen204@gmail.com>
2024-11-22 17:59:09 +0100

    done with the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import React, { useState } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
-import AdbIcon from '@mui/icons-material/Adb';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import "@fontsource/raleway";
 import {
     Badge,
     Box,
@@ -19,11 +19,13 @@ import {
 } from '@mui/material';
 
 const pages = ['Overview', 'Leads', 'Clients', 'Sales'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ['Account', 'Logout'];
 
 function ResponsiveAppBar() {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const [activeTitle, setActiveTitle] = useState("Overview");
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
@@ -52,66 +54,119 @@ function ResponsiveAppBar() {
                         sx={{
                             mr: 2,
                             display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            color: 'inherit',
+                            fontFamily: 'Raleway, sans-serif',
+                            fontStyle: 'italic',
+                            fontWeight: 'bold',
+                            color: '#e18422',
                             textDecoration: 'none',
                         }}
                     >
                         GREENe
                     </Typography>
 
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none', } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{ display: { xs: 'block', md: 'none' } }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography onClick={() => { setActiveTitle(page); }} sx={{
+                                        textAlign: 'center', textTransform: 'initial', color: activeTitle === page ? "#e18422" : "black",
+                                        fontWeight: activeTitle === page ? "bold" : "normal"
+                                    }}>{page}</Typography>
+                                </MenuItem>
+
+                            ))}
+                            <MenuItem onClick={handleCloseNavMenu}> <Typography sx={{ textAlign: 'center', textTransform: 'initial' }}>Email</Typography></MenuItem>
+                            <MenuItem onClick={handleCloseNavMenu}> <Typography sx={{ textAlign: 'center', textTransform: 'initial' }}>Notifications</Typography> </MenuItem>
+                        </Menu>
+
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                                mr: 2,
+                                mt: 1.4,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'Raleway, sans-serif',
+                                fontStyle: 'italic',
+                                fontWeight: 'bold',
+                                color: '#e18422',
+                                textDecoration: 'none',
+                                fontSize: '20px'
+                            }}
+                        >
+                            GREENe
+                        </Typography>
+                    </Box>
 
 
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        GREENe
-                    </Typography>
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', display: 'flex', marginLeft: '70px' }, gap: '30px' }}>
                         {pages.map((page) => (
                             <Button
                                 key={page}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: 'white', display: 'block', textTransform: 'initial' }}
+                                onClick={() => {
+                                    handleCloseNavMenu();
+                                    setActiveTitle(page);
+                                }}
+                                sx={{
+                                    my: 2, color: 'white', display: 'block', textTransform: 'initial', color: activeTitle === page ? "#e18422" : "white",
+                                    fontWeight: activeTitle === page ? "bold" : "normal"
+                                }}
                             >
                                 {page}
                             </Button>
                         ))}
                     </Box>
                     <Box sx={{ flexGrow: 0, display: { xs: 'flex', md: 'flex', display: 'flex' }, gap: { xs: '8px', md: '20px' } }}>
-                        <Box sx={{ flexGrow: 0, display: { xs: 'flex', display: 'flex' }, gap: { xs: '5px' } }}>
+                        <Box sx={{ flexGrow: 0, display: { xs: 'flex', display: 'flex' }, gap: { md: '5px' } }}>
                             <Box>
-                                <IconButton size="medium" aria-label="show 4 new mails" color="inherit">
-                                    <Badge sx={{ backgroundColor: '#404040', borderRadius: '30px', width: { md: '32px', xs: '27px', sm: '30px' }, height: { md: '32px', xs: '27px', sm: '30px' }, display: 'flex', justifyContent: 'center', mt: { xs: 0.5, sm: 0.5 } }}>
-                                        <MailIcon sx={{ mt: { md: 0.7, xs: 0.5, sm: 0.5 }, fontSize: 'large' }} />
+                                <IconButton size="medium" aria-label="show 4 new mails" color="inherit" sx={{ display: { xs: 'none', sm: 'none', md: 'inline' } }}>
+                                    <Badge sx={{ backgroundColor: '#404040', borderRadius: '30px', width: { md: '32px' }, height: { md: '32px' }, display: 'flex', justifyContent: 'center' }}>
+                                        <MailIcon sx={{ mt: { md: 0.7 }, fontSize: 'large' }} />
                                     </Badge>
                                 </IconButton>
                                 <IconButton
                                     size="small"
                                     aria-label="show 17 new notifications"
                                     color="inherit"
+                                    sx={{ display: { xs: 'none', sm: 'none', md: 'inline' } }}
                                 >
-                                    <Badge sx={{ backgroundColor: '#404040', borderRadius: '30px', width: { md: '32px', xs: '26px', sm: '30px' }, height: { md: '32px', xs: '26px', sm: '30px' }, display: 'flex', justifyContent: 'center', mt: { xs: 0.5, sm: 0.5 } }}>
-                                        <NotificationsIcon sx={{ mt: { md: 0.7, xs: 0.4, sm: 0.4 }, fontSize: 'large' }} />
+                                    <Badge sx={{ backgroundColor: '#404040', borderRadius: '30px', width: { md: '32px' }, height: { md: '32px' }, display: 'flex', justifyContent: 'center' }}>
+                                        <NotificationsIcon sx={{ mt: { md: 0.7 }, fontSize: 'large' }} />
                                     </Badge>
                                 </IconButton>
                             </Box>
                             <Tooltip title="Open settings">
                                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt="Joe Jay" src="/static/images/avatar/2.jpg" sx={{ fontSize: 'medium', width: { md: '36px', xs: '28px', sm: '30px' }, height: { md: '36px', xs: '28px', sm: '30px' }, mb: { xs: 0.4, sm: 0.4 } }} />
+                                    <Avatar alt="Joe Jay" src="/static/images/avatar/2.jpg" sx={{ fontSize: 'medium', width: { md: '36px', xs: '28px', sm: '30px' }, height: { md: '36px', xs: '28px', sm: '30px' } }} />
                                 </IconButton>
                             </Tooltip>
                             <Menu
@@ -137,45 +192,16 @@ function ResponsiveAppBar() {
                                 ))}
 
                             </Menu>
+                            <Box sx={{ display: 'block', mt: 1 }}>
+                                <Typography sx={{ display: { xs: 'none', sm: 'none', md: 'flex', display: 'flex' }, fontSize: '12px', fontWeight: 'bold' }}>
+                                    Joy Jay
+                                </Typography>
+                                <Typography sx={{ display: { xs: 'none', sm: 'none', md: 'flex', display: 'flex' }, fontSize: '8px' }}>
+                                    Sales Manager
+                                </Typography>
+                            </Box>
+                        </Box>
 
-                        </Box>
-                        <Typography sx={{ display: { xs: 'none', sm: 'none', md: 'flex', display: 'flex' } }}>
-                            LOL
-                        </Typography>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none', } }}>
-                            <IconButton
-                                size="large"
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleOpenNavMenu}
-                                color="inherit"
-                            >
-                                <MenuIcon sx={{ mb: 0.7}} />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorElNav}
-                                anchorOrigin={{
-                                    vertical: 'bottom',
-                                    horizontal: 'left',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'left',
-                                }}
-                                open={Boolean(anchorElNav)}
-                                onClose={handleCloseNavMenu}
-                                sx={{ display: { xs: 'block', md: 'none' } }}
-                            >
-                                {pages.map((page) => (
-                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                        <Typography sx={{ textAlign: 'center', textTransform: 'initial' }}>{page}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
                     </Box>
                 </Toolbar>
             </Container>
